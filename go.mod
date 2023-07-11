4888811196f4d1ae62ac26db481c6992f643411d
JULIO CESAR ARANDA CONTRERAS <juliocesar.aranda.contreras.next@bbva.com>
2023-07-10 19:15:49 -0600

    subiendo codigo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JulioAranda/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/JulioAranda/godesde0/variables"
+)
+
+func main() {
+	variables.MuestraEnteros()
+}
