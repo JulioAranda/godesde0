8bd8523426a8c5d3e83c682a490903e7acb38ee3
JULIO CESAR ARANDA CONTRERAS <juliocesar.aranda.contreras.next@bbva.com>
2023-07-11 18:26:03 -0600

    12. condiciones en GO

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/JulioAranda/godesde0/variables"
 )
@@ -12,4 +13,23 @@ func main() {
 	estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "linux" || os == "darwin" {
+		fmt.Println("Esto es Mac")
+
+	} else {
+		fmt.Println("Esto es Windows ")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+
+	case "Windows":
+		fmt.Println("Esto es Windows ")
+
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
 }
