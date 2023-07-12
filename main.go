f30adf79cfd8a8db873cb6debcbe16ebe6981926
JULIO CESAR ARANDA CONTRERAS <juliocesar.aranda.contreras.next@bbva.com>
2023-07-11 18:45:47 -0600

    12. condiciones en GO

--- main.go
+++ main.go
@@ -2,34 +2,37 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
-	"github.com/JulioAranda/godesde0/variables"
+	"github.com/JulioAranda/godesde0/ejercicios"
 )
 
 func main() {
 	//variables.MuestraEnteros()
 	//variables.RestoVariables()
-	estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	/*
+		estado, texto := variables.ConviertoaTexto(1588)
+		fmt.Println(estado)
+		fmt.Println(texto)
 
-	if os := runtime.GOOS; os == "linux" || os == "darwin" {
-		fmt.Println("Esto es Mac")
+		if os := runtime.GOOS; os == "linux" || os == "darwin" {
+			fmt.Println("Esto es Mac")
 
-	} else {
-		fmt.Println("Esto es Windows ")
-	}
+		} else {
+			fmt.Println("Esto es Windows ")
+		}
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es Linux")
 
-	case "Windows":
-		fmt.Println("Esto es Windows ")
+		case "Windows":
+			fmt.Println("Esto es Windows ")
 
-	default:
-		fmt.Printf("%s \n", os)
-	}
-
+		default:
+			fmt.Printf("%s \n", os)
+		}
+	*/
+	valor1, valor2 := ejercicios.EvaluaEntero("2")
+	fmt.Println(valor1)
+	fmt.Println(valor2)
 }
