48a9b9eec488e2fd3b09df9514d1747204aa2d03
JULIO CESAR ARANDA CONTRERAS <juliocesar.aranda.contreras.next@bbva.com>
2023-07-11 18:09:16 -0600

    11. Funciones en GO

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/JulioAranda/godesde0/variables"
 )
 
 func main() {
-	variables.MuestraEnteros()
+	//variables.MuestraEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
